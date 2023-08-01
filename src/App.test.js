eb713c31d7aa1abab2083cd7870fe467df110061
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-01 19:25:25 +0200

    Implement integration test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,18 @@
 import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App component',()=>{
+  it('should say enter your text here', async () => {
+    // Arrange
+    const user = userEvent.setup();
+    render(<App/>);
+
+    // Act
+    await user.type(screen.getByLabelText('input'),'Enter your text here');
+
+    // Assert
+    expect(screen.getByTestId("input")).toHaveTextContent('Enter your text here')
+  });
 });
