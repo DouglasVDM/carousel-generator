7aa1b1870f350b2e4d28f60d0b5330f6abe07828
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-01 19:05:53 +0200

    Implement E2E test

--- cypress/e2e/journey.cy.js
+++ cypress/e2e/journey.cy.js
@@ -1,10 +1,12 @@
-it("should say Hello Worldenter your text here", () => {
-  // Arrange
-  cy.visit("/");
+describe("app landing page", () => {
+  it("should say enter your text here", () => {
+    // Arrange
+    cy.visit("/");
 
-  // Act
-  cy.get('input').type('Hello, World')
+    // Act
+    cy.get("input").type("Enter your text here");
 
-  // Assert
-  cy.findByTestId("input").should("contain.text", "");
+    // Assert
+    cy.findByTestId("input").should("contain.text", "Enter your name here!");
+  });
 });
