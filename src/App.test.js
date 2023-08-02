359cae1d7f106562930dc06eece94f469b7f020f
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-02 23:29:03 +0200

    Moved input tests to Pdf component

--- src/App.test.js
+++ src/App.test.js
@@ -1,18 +1,16 @@
 import { render, screen } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
 
 import App from './App';
 
 describe('App component',()=>{
-  it('should say enter your text here', async () => {
+  it('has a container div', async () => {
     // Arrange
-    const user = userEvent.setup();
     render(<App/>);
 
     // Act
-    await user.type(screen.getByLabelText('input'),'Enter your text here');
+    const element = screen.getByTestId('container');
 
     // Assert
-    expect(screen.getByTestId("input")).toHaveTextContent('Enter your text here')
+    expect(element.className).toBe("container")
   });
 });
