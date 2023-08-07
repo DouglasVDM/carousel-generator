dfb9dc534512edebf6e8a6d0aeb207c871dd030f
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-07 21:28:51 +0200

    Moved text input to FileUpload component

--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -1,41 +1,10 @@
-
-import React, { useState } from "react";
+import React from "react";
 import FileUpload from "./FileUpload";
-import { Box, TextField } from "@mui/material";
 
 const Pdf = () => {
-  const [text, setText] = useState("");
-
-  // const onSubmit = ()=>{
-  // }:
-
-  const handleChange = (event) => {
-    event.preventDefault();
-    const pdfText = event.target.value;
-    setText(pdfText);
-  };
-
   return (
     <div className="input-container">
-      <div>
-        <FileUpload />
-      </div>
-      <div>
-        <h1>{text}</h1>
-      </div>
-      <Box>
-        <TextField
-          data-testid="pdf"
-          id="outlined-basic"
-          label="Enter your text"
-          variant="outlined"
-          type="text"
-          name="input-text"
-          value={text}
-          onChange={handleChange}
-          placeholder="Enter your text here"
-        />
-      </Box>
+      <FileUpload />
     </div>
   );
 };
