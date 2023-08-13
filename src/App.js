17dd25079e7342c8b80ba0b74d67a7e0cb15a93a
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-13 20:15:19 +0200

    Removed router

--- src/App.js
+++ src/App.js
@@ -7,24 +7,7 @@ import FileUpload from "./components/FileUpload";
 function App() {
   return (
     <div data-testid="container" className="container">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/parent">Pdf Component</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/parent" exact element={<Pdf />} />
-          <Route path="/parent/:id" element={<FileUpload />} />
-          <Route path="/" element={<h1>Home Page</h1>} />
-        </Routes>
-      </Router>
+      <Pdf/>
     </div>
   );
 };
