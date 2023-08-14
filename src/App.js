c2050f27bbb3ff2249fe085944b00151f8e48306
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-14 21:19:08 +0200

    Removed Router, Route and Link component

--- src/App.js
+++ src/App.js
@@ -1,32 +1,26 @@
+import { useState } from "react";
 import "./App.css";
-import { BrowserRouter as Router, Route, Link, Routes } from "react-router-dom";
-
-import Pdf from "./components/Pdf";
 import FileUpload from "./components/FileUpload";
 
+import Pdf from "./components/Pdf";
+
 function App() {
+  const [duplicatedComponents, setDuplicatedComponents] = useState([]);
+
   return (
     <div data-testid="container" className="container">
-      <Router>
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-            <li>
-              <Link to="/parent">Pdf Component</Link>
-            </li>
-          </ul>
-        </nav>
-
-        <Routes>
-          <Route path="/parent" exact element={<Pdf />} />
-          <Route path="/parent/:id" element={<FileUpload />} />
-          <Route path="/" element={<h1>Home Page</h1>} />
-        </Routes>
-      </Router>
+      <Pdf
+        setDuplicatedComponents={setDuplicatedComponents}
+        components={duplicatedComponents}
+        renderItem={(duplicatedComponent, isHighlighted) => (
+          <FileUpload
+            key={duplicatedComponent.id}
+            isHighlighted={isHighlighted}
+          />
+        )}
+      />
     </div>
   );
-};
+}
 
 export default App;
