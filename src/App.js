9c66f2b9c4846a7091fa8049ee32135be3638f94
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-16 21:57:44 +0200

    Moved functions to App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,34 @@ import Pdf from "./components/Pdf";
 
 function App() {
   const [duplicatedComponents, setDuplicatedComponents] = useState([]);
+  const [selectedIndex, setSelectedIndex] = useState(0);
+
+  const handleDuplicate = () => {
+    const newComponent = <FileUpload />;
+    console.log("duplicated components b4: ", duplicatedComponents);
+
+    setDuplicatedComponents([...duplicatedComponents, newComponent]);
+    console.log("duplicated components after: ", duplicatedComponents);
+  };
 
   return (
     <div data-testid="container" className="container">
+      <br />
+      <button onClick={() => handleDuplicate()}>Duplicate Component</button>
+      {duplicatedComponents.map((component, index) => {
+        console.log(component);
+        const isHighlighted = index === selectedIndex;
+        return setSelectedIndex(component, isHighlighted);
+      })}
+      <hr />
+      <button
+        onClick={() => {
+          setSelectedIndex((index) => (index + 1) % duplicatedComponents.length);
+        }}
+      >
+        Next
+      </button>
       <Pdf
-        setDuplicatedComponents={setDuplicatedComponents}
-        components={duplicatedComponents}
         renderItem={(duplicatedComponent, isHighlighted) => (
           <FileUpload
             key={duplicatedComponent.id}
--- src/components/Pdf.js
+++ src/components/Pdf.js
@@ -2,34 +2,10 @@ import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import FileUpload from "./FileUpload";
 
-const Pdf = ({ setDuplicatedComponents, components, renderItem }) => {
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const handleDuplicate = () => {
-    const newComponent = <FileUpload />;
-    console.log("duplicated components b4: ", components);
-    setDuplicatedComponents([...components, newComponent]);
-    console.log("duplicated components after: ", components);
-  };
-
+const Pdf = ({ components }) => {
   return (
     <div className="input-container">
-      {/* <FileUpload /> */}
-      <br />
-      <button onClick={()=>handleDuplicate()}>Duplicate Component</button>
-      {components.map((component, index) => {
-        console.log(component);
-        const isHighlighted = index === selectedIndex;
-        return renderItem(component,isHighlighted);
-      })}
-      <hr />
-      <button
-        onClick={() => {
-          setSelectedIndex((index) => (index + 1) % components.length);
-        }}
-      >
-        Next
-      </button>
+      <FileUpload />
     </div>
   );
 };
