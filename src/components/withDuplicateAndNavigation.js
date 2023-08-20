a325b7e00741cf74a51221e7493e00275f72bb80
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-20 06:54:07 +0200

    Documented the functionality

--- src/components/withDuplicateAndNavigation.js
+++ src/components/withDuplicateAndNavigation.js
@@ -1,11 +1,34 @@
+/**
+ * The `DuplicateAndNavigationFileUpload` component is created by wrapping the `FileUpload` component with the `withDuplicateAndNavigation` HOC.  
+ * The `DuplicateAndNavigationFileUpload` component can now be used in other parts of your application with the added functionality of both duplicate and navigation.  
+ * The buttons for next and previous navigation will be added to the duplicated components as well.
+ */
 import React from "react";
 import withDuplicate from "./withDuplicate";
-import withNavigation from "./withNavigation";
+import { useNavigate } from "react-router-dom";
 
 const withDuplicateAndNavigation = (WrappedComponent) => {
-  const DuplicateAndNavigationComponent = withDuplicate(
-    withNavigation(WrappedComponent)
-  );
+  const DuplicateAndNavigationComponent = withDuplicate((props) => {
+    const navigate = useNavigate();
+
+    const handleNext = () => {
+      const nextId = parseInt(props.match.params.id) + 1;
+      navigate(`/components/${nextId}`);
+    };
+
+    const handlePrevious = () => {
+      const previousId = parseInt(props.match.params.id) - 1;
+      navigate(`/components/${previousId}`);
+    };
+
+    return (
+      <div>
+        <WrappedComponent {...props} />
+        <button onClick={handlePrevious}>Previous</button>
+        <button onClick={handleNext}>Next</button>
+      </div>
+    );
+  });
 
   return DuplicateAndNavigationComponent;
 };
