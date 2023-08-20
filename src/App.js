dc66184d603848ccf75808a166485829b95d56d7
Douglas Van Der Merwe <74470226+DouglasVDM@users.noreply.github.com>
2023-08-20 06:17:17 +0200

    Import WithDuplicateAndNavigation component

--- src/App.js
+++ src/App.js
@@ -1,42 +1,16 @@
 import { useState } from "react";
 import "./App.css";
 import FileUpload from "./components/FileUpload";
+import withDuplicateAndNavigation from "./components/withDuplicateAndNavigation";
 
-import Pdf from "./components/Pdf";
+const DuplicateAndNavigationFileUpload = withDuplicateAndNavigation(FileUpload);
 
 function App() {
-  const [duplicatedComponents, setDuplicatedComponents] = useState([]);
-  const [selectedIndex, setSelectedIndex] = useState(0);
-
-  const handleDuplicate = () => {
-    const newComponent = <FileUpload />;
-    console.log("duplicated components b4: ", duplicatedComponents);
-
-    setDuplicatedComponents([...duplicatedComponents, newComponent]);
-    console.log("duplicated components after: ", duplicatedComponents);
-  };
-
   return (
     <div data-testid="container" className="container">
-      <br />
-      <button onClick={() => handleDuplicate()}>Duplicate Component</button>
-      {duplicatedComponents.map((component, index) => {
-        console.log(component);
-        const isHighlighted = index === selectedIndex;
-        return setSelectedIndex(component, isHighlighted);
-      })}
-      <hr />
-      <button
-        onClick={() => {
-          setSelectedIndex((index) => (index + 1) % duplicatedComponents.length);
-        }}
-      >
-        Next
-      </button>
-      <Pdf
-      />
+      <DuplicateAndNavigationFileUpload />
     </div>
   );
-}
+};
 
 export default App;
